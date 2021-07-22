f2165e594c98c913798f6ae988eed83cb94eaee6
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-07-22 21:38:19 +0700

    update code

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,3 +1,4 @@
+import { Button, Input, NativeBaseProvider, Radio } from 'native-base';
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
@@ -5,10 +6,38 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const [value, setValue] = React.useState("one")
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <NativeBaseProvider>
+        <Input
+          variant="outline"
+          placeholder="Outline"
+          _light={{
+            placeholderTextColor: "blueGray.400",
+          }}
+          _dark={{
+            placeholderTextColor: "blueGray.50",
+          }}
+        />
+        <Radio.Group
+          name="myRadioGroup"
+          value={value}
+          onChange={(nextValue) => {
+            setValue(nextValue)
+          }}
+        >
+          <Radio value="one" my={1}>
+            One
+          </Radio>
+          <Radio value="two" my={1}>
+            Two
+          </Radio>
+        </Radio.Group>
+        <Button onPress={() => console.log(value)}>PRIMARY</Button>
+      </NativeBaseProvider>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
