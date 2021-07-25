1cde2b332e8705d22fa880432e3268047ed226dc
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-07-25 22:37:43 +0700

    update code

--- components/ProductSuggestList.tsx
+++ components/ProductSuggestList.tsx
@@ -1,71 +1,35 @@
 import React from "react"
 import { FlatList, Center, NativeBaseProvider, Text, Box } from "native-base"
 import ProductSuggestCard from "./ProductSuggestCard"
-import { View } from "react-native"
-export function ProductSuggestList() {
-  const data = [
-    {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-      title: "First Item",
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-      title: "Second Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d72",
-      title: "Third Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d73",
-      title: "Four Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d74",
-      title: "Five Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d75",
-      title: "Six Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d76",
-      title: "Third Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d77",
-      title: "Third Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d78",
-      title: "Third Item",
-    },
-  ]
+import { SafeAreaView, View } from "react-native"
+export function ProductSuggestList(data: any) {
+  const itemList = data.data.data;
 
   return (
     <>
       <View style={{ flexDirection: 'row' }}>
-        <Text style={{ marginLeft: 5, flex: 1, fontWeight: 'bold', }}>Sản phẩm gợi ý</Text>
-        <Text style={{ textAlign: 'right', flex: 1, marginRight: 5 }} onPress={() => console.log("hello world")}>Xem thêm</Text>
+        <Text style={{ marginLeft: 5, flex: 1, fontWeight: 'bold', }}>SẢN PHẨM GỢI Ý</Text>
       </View>
-      <FlatList
-        horizontal={true}
-        data={data}
-        height={300}
-        renderItem={({ item }) => (
-          <ProductSuggestCard />
-        )}
-        keyExtractor={(item) => item.id} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList
+          horizontal={true}
+          data={itemList}
+          height={300}
+          renderItem={({ item }) => (
+            <ProductSuggestCard data={item} />
+          )}
+          keyExtractor={(item) => item.id} />
+      </SafeAreaView>
     </>
   )
 }
 
-export default () => {
+export default (data: any) => {
   return (
     <NativeBaseProvider>
       <Box bg="white"
         shadow={2}>
-        <ProductSuggestList />
+        <ProductSuggestList data={data} />
       </Box>
     </NativeBaseProvider>
   )
