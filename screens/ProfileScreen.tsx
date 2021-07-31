cc2aca86d1c3dd6a214e788411aecf7124bd06bc
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-07-31 17:15:18 +0700

    update code

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,10 +1,42 @@
+import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import ApiCommon from '../constants/ApiCommon';
 
 export default function ProfileScreen() {
+    const [userLogin, setUserLogin] = useState<any>({});
+    const [isLoading, setLoading] = useState(false);
+    const [value, setValue] = useState<string | null>('');
+    const { getItem, setItem } = useAsyncStorage('token');
+    const [retrieve, setRetrieve] = useState(true);
+
+    useEffect(() => {
+        const readToken = async () => {
+            const item = await getItem();
+            console.log('item: ' + item);
+            setValue(item);
+            setRetrieve(false);
+        };
+
+        if (retrieve) {
+            readToken();
+        }
+        if (retrieve === false) {
+            console.log('token' + value)
+            const headers = { 'Authorization': `Bearer ${value}` }
+            fetch(ApiCommon.rootUrl + '/api/user/login', { headers })
+                .then((response) => response.json())
+                .then((json) => console.log(json))
+                .catch((error) => console.error(error));
+        }
+
+    }, [retrieve]);
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Cá nhân!</Text>
         </View>
     );
 }
+
