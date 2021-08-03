e6a3fccb627462510c5a0b916c44603d5e11a9ca
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-08-03 23:20:41 +0700

    update code

--- screens/NotifyScreen.tsx
+++ screens/NotifyScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react"
-import { Box, FlatList, Center, NativeBaseProvider, Icon } from "native-base"
+import { Box, FlatList, Center, NativeBaseProvider, Icon, View, Image, Text } from "native-base"
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import ApiCommon from "../constants/ApiCommon";
-import { TouchableRipple } from "react-native-paper";
 import { Modal, Button } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 export default function NotifyScreen() {
   const { getItem, setItem } = useAsyncStorage('token');
   const [retrieve, setRetrieve] = useState(true);
@@ -48,11 +49,41 @@ export default function NotifyScreen() {
         <FlatList
           data={itemNotifi}
           renderItem={({ item }) => (
-            <TouchableRipple onPress={() => { openModal("top"), setContent(item.content) }}>
-              <Box width={300} px={5} py={2} rounded="md" my={1} bg="primary.300">
-                {item.title}
-              </Box>
-            </TouchableRipple>
+            <TouchableOpacity onPress={() => { openModal("top"), setContent(item.content) }}>
+              <View style={{ flexDirection: "row", marginTop: '5%' }}>
+                <View style={{ width: '15%', alignContent: 'center', alignItems: 'center' }}>
+                  <Image source={require('../assets/images/MiMartLogoGradientApp.png')} alt="image base" resizeMode="cover" width='80%' height={45} rounded="80" />
+                  {item.notifyType == 1 &&
+                    <Center
+                      p={1}
+                      rounded="full"
+                      bg='#FFCC66'
+                      boxSize={25}
+                      position="absolute"
+                      right={0}
+                      m={2}
+                      top={3}
+                      left={6}
+                      _text={{
+                        color: "white",
+                        textAlign: "center",
+                        fontWeight: "700",
+                        fontSize: "xs",
+                      }}
+                    >
+                      <FontAwesome name='dropbox' color='#fff' size={18} />
+                    </Center>
+                  }
+                </View>
+                <View style={{ width: '85%' }}>
+                  <Box width='100%' px={5} py={2} rounded="lg" bg="primary.300" height={45} backgroundColor='#FFCC66'>
+                    <Text>{item.title}</Text>
+                  </Box>
+                </View>
+
+              </View>
+
+            </TouchableOpacity>
           )}
           keyExtractor={(item) => item.id}
         />
