7debbe7cac6272d48b1fecafeb5a61b68b197361
NguyenVanLuong95 <nguyenluong040595@gmail.com>
2021-08-03 22:10:49 +0700

    modal notification

--- screens/NotifyScreen.tsx
+++ screens/NotifyScreen.tsx
@@ -71,7 +71,7 @@ export default function NotifyScreen() {
                   setOpen(false)
                 }}
               >
-                OK
+                Đóng
               </Button>
             </Button.Group>
           </Modal.Footer>
