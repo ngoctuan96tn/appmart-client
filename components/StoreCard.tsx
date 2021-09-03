e6ab163323cc0ab3ed516871e44fd149b052478d
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-09-03 22:46:30 +0700

    update store list

--- components/StoreCard.tsx
+++ components/StoreCard.tsx
@@ -2,26 +2,30 @@ import React from "react";
 import {
   Image, Text, NativeBaseProvider, Center, Box, Stack, Heading, Button
 } from "native-base";
-function CategoryCard() {
+import { TouchableOpacity } from "react-native";
+function CategoryCard(data: any) {
+  const store = data.data.data;
   return (
-    <Box
-      bg="white"
-      shadow={2}
-      rounded="lg"
-      width={120}
-      marginLeft={1}
-      marginRight={1}
-    >
-      <Image source={require('../assets/images/MiMartLogoGradientBlue.png')} alt="image base" resizeMode="cover" height={150} roundedTop="md" />
-      <Text color='black' left={1}>Mi Mart</Text>
-    </Box>
+    <TouchableOpacity onPress={() => console.log('hello')}>
+      <Box
+        bg="white"
+        shadow={2}
+        rounded="lg"
+        width={120}
+        marginLeft={1}
+        marginRight={1}
+      >
+        <Image source={{ uri: `data:image/jpeg;base64,${store.image}` }} alt="image base" resizeMode="cover" height={150} roundedTop="md" />
+        <Text color='black' left={1}>{store.name}</Text>
+      </Box>
+    </TouchableOpacity>
   );
 }
 
-export default function () {
+export default function (data: any) {
   return (
     <NativeBaseProvider>
-        <CategoryCard />
+      <CategoryCard data={data} />
     </NativeBaseProvider>
   );
 }
--- components/StoreList.tsx
+++ components/StoreList.tsx
@@ -1,26 +1,34 @@
 import React from "react"
-import { HStack, Stack, Center, NativeBaseProvider, Text, Box } from "native-base"
+import { HStack, Stack, Center, NativeBaseProvider, Text, Box, FlatList } from "native-base"
 import StoreCard from "./StoreCard"
 import { SafeAreaView, View } from "react-native"
-export function StoreList() {
+export function StoreList(data: any) {
+    const dataStore = data.data.listData;
     return (
         <><View style={{ flexDirection: 'row' }}>
             <Text style={{ marginLeft: 5, flex: 1, fontWeight: 'bold', }}>CỬA HÀNG</Text>
             <Text style={{ textAlign: 'right', flex: 1, marginRight: 5, fontWeight: 'bold', color: '#ffa500' }}>Xem thêm</Text>
         </View>
-        <SafeAreaView style={{ flex: 1, marginTop: 10 }}>
-            <StoreCard />
-        </SafeAreaView>    
+
+            <SafeAreaView style={{ flex: 1, marginTop: 10 }}>
+                <FlatList
+                    data={dataStore}
+                    renderItem={({ item }) => (
+                        <StoreCard data={item} />
+                    )}
+                    numColumns={3}
+                    keyExtractor={(item) => item.id.toString()} />
+            </SafeAreaView>
         </>
     )
 }
 
-export default () => {
+export default (data: any) => {
     return (
         <NativeBaseProvider>
             <Box bg="white"
                 shadow={2}>
-                <StoreList />
+                <StoreList data={data} />
             </Box>
         </NativeBaseProvider>
     )
