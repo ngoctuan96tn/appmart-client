43de57a8eba5d1058be7c0c4f8f0d6dcdf35d741
NguyenVanLuong95 <nguyenluong040595@gmail.com>
2021-10-23 11:44:24 +0700

    update code

--- components/StoreList.tsx
+++ components/StoreList.tsx
@@ -7,12 +7,12 @@ export function StoreList(data: any) {
     const dataStore = data.data.listData;
     const navigation = useNavigation();
     return (
-        <><View style={{ flexDirection: 'row' }}>
+        <><View style={{ flexDirection: 'row', backgroundColor: '#fff' }}>
             <Text style={{ marginLeft: '3%', flex: 1, fontWeight: 'bold', }}>CỬA HÀNG</Text>
             <Text style={{ textAlign: 'right', flex: 1, marginRight: 5, fontWeight: '300', color: '#ffa500' }} onPress={() => {navigation.navigate('ListStores')}}>Xem thêm</Text>
         </View>
 
-            <SafeAreaView style={{ flex: 1, marginTop: '5%', marginBottom:'2%', marginLeft: '2%' }}>
+            <SafeAreaView style={{ flex: 1, marginTop: '5%', marginBottom:'2%', marginLeft: '2%', backgroundColor:'#fff' }}>
                 <FlatList
                     data={dataStore}
                     renderItem={({ item }) => (
