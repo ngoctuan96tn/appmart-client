c51a786a47e1b8b680aee906eb2065396f24afb8
DESKTOP-2D1P8OH\Admin <tuandev1996@gmail.com>
2021-11-28 21:01:46 +0700

    update code

--- screens/ListProduct.tsx
+++ screens/ListProduct.tsx
@@ -27,7 +27,7 @@ export function ListProduct(route: any) {
         const limitCalculate = limit + 6;
         const offset = limit;
         setLimit(limitCalculate);
-        fetch(ApiCommon.rootUrl + `/api/products/category-paging/${params.categoryId}?limit=${limit}&offset=${offset}`)
+        fetch(ApiCommon.rootUrl + `/api/products/category-paging/${params.categoryId}?limit=6&offset=${offset}`)
             .then((response) => response.json())
             .then((json) => {
                 console.log(data[data.length-1].productId);
